ff955ad588eff54194ba87882e710b451eee5ca0
zhaoyan <172115282@qq.com>
2018-09-04 23:04:21 +0800

    分类管理

--- resources/views/layouts/admin/_menu.blade.php
+++ resources/views/layouts/admin/_menu.blade.php
@@ -106,27 +106,6 @@
         </ul>
     </li>
     <!-- 分类开始 -->
-    <!-- <li class="tpl-left-nav-item">
-        <a href="javascript:;" class="nav-link tpl-left-nav-link-list">
-            <i class="am-icon-wpforms"></i>
-            <span>分类管理</span>
-            <i class="am-icon-angle-right tpl-left-nav-more-ico am-fr am-margin-right tpl-left-nav-more-ico-rotate"></i>
-        </a>
-        <ul class="tpl-left-nav-sub-menu" style="display: none">
-            <li>
-                <a href="/cate_1/create">
-                    <i class="am-icon-angle-right"></i>
-                    <span>分类添加</span>
-                    <i class="tpl-left-nav-content-ico am-fr am-margin-right"></i>
-                </a>
-                <a href="/cate_1">
-                    <i class="am-icon-angle-right"></i>
-                    <span>分类列表</span>
-                </a>
-            </li>
-        </ul>
-    </li>
- -->
     </li>
         <li class="tpl-left-nav-item">
         <a href="javascript:;" class="nav-link tpl-left-nav-link-list">
