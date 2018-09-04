e689103b4048d9b347bc96364b9e75678db6ff6c
weidong <65373489@qq.com>
2018-09-04 10:29:16 +0800

    第一次修改

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username')->comment('账号');
+            $table->string('password')->comment('密码');
+            $table->string('name')->comment('昵称');
+            $table->string('phone')->comment('手机号');
+            $table->string('image')->comment('头像');
+            $table->string('sex')->comment('性别');
+            $table->string('site')->comment('地址');
+            $table->string('jifen')->comment('积分');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
