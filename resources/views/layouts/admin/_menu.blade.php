d39ad093faf84d0d34715c683689fc5b4ad7100b
yuxuan <476760027@qq.com>
2018-09-04 17:08:52 +0800

    ...

--- resources/views/layouts/admin/_menu.blade.php
+++ resources/views/layouts/admin/_menu.blade.php
@@ -24,20 +24,20 @@
     <li class="tpl-left-nav-item">
         <a href="javascript:;" class="nav-link tpl-left-nav-link-list">
             <i class="am-icon-wpforms"></i>
-            <span>文章管理</span>
+            <span>商品管理</span>
             <i class="am-icon-angle-right tpl-left-nav-more-ico am-fr am-margin-right tpl-left-nav-more-ico-rotate"></i>
         </a>
         <ul class="tpl-left-nav-sub-menu" style="display: none;">
             <li>
-                <a href="/article/create">
+                <a href="/good/create">
                     <i class="am-icon-angle-right"></i>
-                    <span>文章添加</span>
+                    <span>商品添加</span>
                     <i class="tpl-left-nav-content-ico am-fr am-margin-right"></i>
                 </a>
 
-                <a href="/article">
+                <a href="/good">
                     <i class="am-icon-angle-right"></i>
-                    <span>文章列表</span>
+                    <span>商品列表</span>
                 </a>
             </li>
         </ul>
