3767927a77d9e5277ac2613e06ec71c906826e38
yuxuan <476760027@qq.com>
2018-09-05 22:57:51 +0800

    .....

--- resources/views/admin/cates/index.blade.php
+++ resources/views/admin/cates/index.blade.php
@@ -1,14 +1,10 @@
 @extends('layouts.admin') @section('title','分类列表') @section('content')
-<style>
-.tpl-portlet-components {
-    height: 800px;
-}
-</style>
 <div class="tpl-portlet-components">
     <div class="portlet-title">
         <div class="caption font-green bold">
             <span class="am-icon-code"></span> 列表
         </div>
+        
     </div>
     <div class="tpl-block">
         <div class="am-g">
@@ -20,71 +16,84 @@
                     </div>
                 </div>
             </div>
+            
             <div class="am-u-sm-12 am-u-md-3">
-                <form action="/cates" method="get">
-                    <div class="am-input-group am-input-group-sm">
-                        <input type="text" name="keywords" class="am-form-field" value="{{request()->keywords}}">
-                        <span class="am-input-group-btn">
-                        <button class="am-btn  am-btn-default am-btn-success tpl-am-btn-success am-icon-search"></button>
-                      </span>
-                    </div>
+            	<form action="/cates" method="get">
+                <div class="am-input-group am-input-group-sm">
+                    <input type="text" name="keywords" class="am-form-field" value="{{request()->keywords}}">
+                    <span class="am-input-group-btn">
+			            <button class="am-btn  am-btn-default am-btn-success tpl-am-btn-success am-icon-search"></button>
+			          </span>
+                </div>
                 </form>
             </div>
         </div>
-        
-            
-            <div class="am-g">
-                <div class="am-u-sm-12">
-                    <table class="am-table am-table-striped am-table-hover table-main">
-                        <thead>
-                            <tr>
-                                <th class="table-check">
-                                    <input type="checkbox" class="tpl-table-fz-check">
-                                </th>
+        <div class="am-collapse am-topbar-collapse" id="topbar-collapse">
 
-                                <th class="table-title"><li class="am-dropdown" data-am-dropdown="" data-am-dropdown-toggle="">
+            <ul class="am-nav am-nav-pills am-topbar-nav am-topbar-center admin-header-list tpl-header-list">
+                <li class="am-dropdown" data-am-dropdown="" data-am-dropdown-toggle="">
                     <a class="am-dropdown-toggle tpl-header-list-link" href="javascript:;">
-                         一级分类名 (点击显示) <span class="am-badge tpl-badge-success am-round"></span>
+                         一级分类名 <span class="am-badge tpl-badge-success am-round"></span>
                     </a>
                     <ul class="am-dropdown-content tpl-dropdown-content">
-                        @foreach($cates as $v) @if($v['parent_id']==0)
                         <li class="tpl-dropdown-content-external">
-                            <span class="tpl-color-success">{{$v['name']}}</span>
-                            <div class="am-btn-toolbar">
-                                <a href="/cates/{{$v['id']}}/edit"> 编辑</a>
-                                <form style="float:left" action="/cates/{{$v['id']}}" method="post">
-                                    {{method_field('DELETE')}} {{csrf_field()}}
-                                    <button> 删除</button>
-                                </form>
-                        </li>
-                        @endif @endforeach
+                            <span class="tpl-color-success">5</span><a href="###">全部</a></li>
+                        
                     </ul>
-                </li></th>
-                                <th class="table-title"><li class="am-dropdown" data-am-dropdown="" data-am-dropdown-toggle="">
-                    <a class="am-dropdown-toggle tpl-header-list-link" href="javascript:;">
-                         二级分类名 (点击显示) <span class="am-badge tpl-badge-success am-round"></span>
-                    </a>
-                    <ul class="am-dropdown-content tpl-dropdown-content">
-                        @foreach($cates as $v) @if(!$v['parent_id']==0)
-                        <li class="tpl-dropdown-content-external">
-                            <span class="tpl-color-success">{{$v['name']}}</span>
-                            <div class="am-btn-toolbar">
-                                <a href="/cates/{{$v['id']}}/edit"> 编辑</a>
-                                <form style="float:left" action="/cates/{{$v['id']}}" method="post">
-                                    {{method_field('DELETE')}} {{csrf_field()}}
-                                    <button> 删除</button>
-                                </form>
-                        </li>
-                        @endif @endforeach
-                    </ul>
-                </li></th>
+                </li>
+                
+            </ul>
+        </div>
+        <div class="am-g">
+            <div class="am-u-sm-12">
+                <table class="am-table am-table-striped am-table-hover table-main">
+                    <thead>
+                        <tr>
+                            <th class="table-check">
+                                <input type="checkbox" class="tpl-table-fz-check">
+                            </th>
+                            <th class="table-id">ID</th>
+                            <th class="table-title">一级分类名</th>
+                            <th class="table-title">二级分类名</th>
+                            <th class="table-set">操作</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($cates as $v)
+                        <tr>
+                            <td>
+                                <input type="checkbox">
+                            </td>
+                            <td>{{$v['id']}}</td>
 
-                            </tr>
-                        </thead>
-                    </table>
-                </div>
+                            <td >@if(!$v['parent_id']) {{$v['name']}} @endif</td>
+                            
+                            <td >@foreach($cates as $val) @if($v['parent_id']==$val['id']) {{$v['name']}} @endif @endforeach</td>
+                            
+                            
+                        
+                            
+                            <td>
+                                <div class="am-btn-toolbar">
+                                    <div class="am-btn-group am-btn-group-xs">
+                                        <a href="/cates/{{$v['id']}}/edit" class="am-btn am-btn-default am-btn-xs am-text-secondary"><span class="am-icon-pencil-square-o"></span> 编辑</a>
+                                        <form style="float:left" action="/cates/{{$v['id']}}" method="post">
+                                            {{method_field('DELETE')}}
+                                            {{csrf_field()}}
+                                            <button class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only"><span class="am-icon-trash-o"></span> 删除</button>
+                                        </form>
+
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                
             </div>
         </div>
-        <div class="tpl-alert"></div>
     </div>
-    @endsection
+    <div class="tpl-alert"></div>
+</div>
+@endsection
