7e7e0324e3d87ba53b790d35e9b6ac78fe3faacf
zhaoyan <172115282@qq.com>
2018-09-10 11:59:03 +0800

    完善详情

--- resources/views/home/xiang/index.blade.php
+++ resources/views/home/xiang/index.blade.php
@@ -1,7 +1,43 @@
-@extends('home.jicheng.touti') @section('content')
- <script type="text/javascript" src="/dianji/js/jquery.1.4.2-min.js"></script>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Single</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Classic Style Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="/qiantai/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="/qiantai/css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- js -->
+<script src="/qiantai/js/jquery.min.js"></script>
+<!-- //js -->
+<!-- cart -->
+<script src="/qiantai/js/simpleCart.min.js"></script>
+<!-- cart -->
+<!-- for bootstrap working -->
+<script type="text/javascript" src="/qiantai/js/bootstrap-3.1.1.min.js"></script>
+<!-- //for bootstrap working -->
+<!-- animation-effect -->
+<link href="/qiantai/css/animate.min.css" rel="stylesheet"> 
+<script src="/qiantai/js/wow.min.js"></script>
+<script>
+ new WOW().init();
+</script>
+<!-- //animation-effect -->
+<link href='https://fonts.googleapis.com/css?family=Cabin:400,500,600,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Lato:400,100,300,700,900' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="/dianji/js/jquery.1.4.2-min.js"></script>
+</head>
+	
 <body>
-	 <style type="text/css">
+
+	<style type="text/css">
     * {
         margin: 0;
         padding: 0;
@@ -25,7 +61,7 @@
         height: 390px;
         overflow: hidden;
         position: relative;
-        margin: 100px auto 0 auto;
+        margin: 0px auto 0 50px;
     }
 
     #imageFlow .diapo {
@@ -134,13 +170,186 @@
         cursor: pointer;
     }
     </style>
+<!-- header -->
+	<div class="header">
+			<div class="header-grid">
+					<div class="container">
+				<div class="header-left animated wow fadeInLeft" data-wow-delay=".5s">
+					<ul>
+					<li><i class="glyphicon glyphicon-headphones"></i><a href="#">24x7 live support</a></li>
+						<li><i class="glyphicon glyphicon-envelope" ></i><a href="mailto:info@example.com">@example.com</a></li>
+						<li><i class="glyphicon glyphicon-earphone" ></i>+1234 567 892</li>
+						
+					</ul>
+				</div>
+				<div class="header-right animated wow fadeInRight" data-wow-delay=".5s">
+				<div class="header-right1 ">
+					<ul>
+		
+						<li><i class="glyphicon glyphicon-log-in" ></i><a href="login.html">Login</a></li>
+						<li><i class="glyphicon glyphicon-book" ></i><a href="register.html">Register</a></li>
+					</ul>
+				</div>
+				<div class="header-right2">
+					<div class="cart box_1">
+						<a href="checkout.html">
+							<h3> <div class="total">
+								<span class="simpleCart_total"></span> (<span id="simpleCart_quantity" class="simpleCart_quantity"></span> items)</div>
+								<img src="/qiantai/images/cart.png" alt="" />
+							</h3>
+						</a>
+						<p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
+						<div class="clearfix"> </div>
+					</div>	
+				</div>
+				<div class="clearfix"> </div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			</div>
+			<div class="container">
+			<div class="logo-nav">
+				
+					<nav class="navbar navbar-default">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header nav_2">
+						<button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						 <div class="navbar-brand logo-nav-left wow fadeInLeft animated" data-wow-delay=".5s">
+							<h1 class="animated wow pulse" data-wow-delay=".5s"><a href="index.html">Classic<span>Style</span></a></h1>
+						</div>
+
+					</div> 
+					<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+						<ul class="nav navbar-nav">
+							<li ><a href="index.html" class="act">Home</a></li>	
+							<!-- Mega Menu -->
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Women <b class="caret"></b></a>
+								<ul class="dropdown-menu multi">
+									<div class="row">
+										<div class="col-sm-4">
+											<ul class="multi-column-dropdown">
+												<h6>Submenu1</h6>
+												
+												<li><a href="products.html">Accessories</a></li>
+												<li><a href="products.html">Bags</a></li>
+												<li><a href="products.html">Caps & Hats</a></li>
+												<li><a href="products.html">Hoodies & Sweatshirts</a></li>
+										
+											</ul>
+										</div>
+										<div class="col-sm-4">
+											<ul class="multi-column-dropdown">
+												<h6>Submenu2</h6>
+												<li><a href="products.html">Jackets & Coats</a></li>
+												<li><a href="products.html">Jeans</a></li>
+												<li><a href="products.html">Jewellery</a></li>
+												<li><a href="products.html">Jumpers & Cardigans</a></li>
+												<li><a href="products.html">Leather Jackets</a></li>
+												<li><a href="products.html">Long Sleeve T-Shirts</a></li>
+
+											</ul>
+										</div>
+										<div class="col-sm-4">
+											<ul class="multi-column-dropdown">
+												<h6>Submenu3</h6>
+												<li><a href="products.html">Shirts</a></li>
+												<li><a href="products.html">Shoes, Boots & Trainers</a></li>
+												<li><a href="products.html">Sunglasses</a></li>
+												<li><a href="products.html">Sweatpants</a></li>
+												<li><a href="products.html">Swimwear</a></li>
+												<li><a href="products.html">Trousers & Chinos</a></li>
+
+											</ul>
+										</div>
+										<div class="clearfix"></div>
+									</div>
+									<div class="row-top">
+										<div class="col-sm-6 row1">
+											<a href="products.html"><img src="/qiantai/images/me.jpg" alt="" class="img-responsive"></a>
+										</div>
+										<div class=" col-sm-6 row2">
+											<a href="products.html"><img src="/qiantai/images/me1.jpg" alt="" class="img-responsive"></a>
+										</div>
+										<div class="clearfix"></div>
+									</div>
+								</ul>
+							</li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Men <b class="caret"></b></a>
+								<ul class="dropdown-menu multi multi1">
+									<div class="row">
+										<div class="col-sm-4">
+											<ul class="multi-column-dropdown">
+												<h6>Submenu1</h6>
+												
+												<li><a href="products1.html">Accessories</a></li>
+												<li><a href="products1.html">Bags</a></li>
+												<li><a href="products1.html">Caps & Hats</a></li>
+												<li><a href="products1.html">Hoodies & Sweatshirts</a></li>
+										
+											</ul>
+										</div>
+										<div class="col-sm-4">
+											<ul class="multi-column-dropdown">
+												<h6>Submenu2</h6>
+												<li><a href="products1.html">Jackets & Coats</a></li>
+												<li><a href="products1.html">Jeans</a></li>
+												<li><a href="products1.html">Jewellery</a></li>
+												<li><a href="products1.html">Jumpers & Cardigans</a></li>
+												<li><a href="products1.html">Leather Jackets</a></li>
+												<li><a href="products1.html">Long Sleeve T-Shirts</a></li>
+
+											</ul>
+										</div>
+										<div class="col-sm-4">
+											<ul class="multi-column-dropdown">
+												<h6>Submenu3</h6>
+												<li><a href="products1.html">Shirts</a></li>
+												<li><a href="products1.html">Shoes, Boots & Trainers</a></li>
+												<li><a href="products1.html">Sunglasses</a></li>
+												<li><a href="products1.html">Sweatpants</a></li>
+												<li><a href="products1.html">Swimwear</a></li>
+												<li><a href="products1.html">Trousers & Chinos</a></li>
+
+											</ul>
+										</div>
+										<div class="clearfix"></div>
+									</div>
+									<div class="row-top">
+										<div class="col-sm-6 row1">
+											<a href="products1.html"><img src="/qiantai/images/me2.jpg" alt="" class="img-responsive"></a>
+										</div>
+										<div class=" col-sm-6 row2">
+											<a href="products1.html"><img src="/qiantai/images/me3.jpg" alt="" class="img-responsive"></a>
+										</div>
+										<div class="clearfix"></div>
+									</div>
+								</ul>
+							</li>
+							
+							<li><a href="codes.html"> Codes</a></li>
+							<li><a href="contact.html">Contact Us</a></li>
+						</ul>
+					</div>
+					</nav>
+				</div>
+				
+		</div>
+	</div>
+<!-- //header -->
 <!--banner-->
 <div class="banner-top">
-    <div class="container">
-        <h2 class="animated wow fadeInLeft" data-wow-delay=".5s">Single</h2>
-        <h3 class="animated wow fadeInRight" data-wow-delay=".5s"><a href="index.html">Home</a><label>/</label>Single</h3>
-        <div class="clearfix"> </div>
-    </div>
+	<div class="container">
+		<h2 class="animated wow fadeInLeft" data-wow-delay=".5s">Single</h2>
+		<h3 class="animated wow fadeInRight" data-wow-delay=".5s"><a href="index.html">Home</a><label>/</label>Single</h3>
+		<div class="clearfix"> </div>
+	</div>
 </div>
 	<!--content-->
 		<div class="product">
@@ -251,7 +460,7 @@
 		<div class="flexslider">
 			  <ul class="slides">
 			    <li data-thumb="/qiantai/images/si.jpg">
-			        <div class="thumb-image"> <img src="C:/xampp/htdocssi/7.jpg" data-imagezoom="true" class="img-responsive"> </div>
+			        <div class="thumb-image"> <img src="/qiantai/images/si.jpg" data-imagezoom="true" class="img-responsive"> </div>
 			    </li>
 			    <li data-thumb="/qiantai/images/si1.jpg">
 			         <div class="thumb-image"> <img src="/qiantai/images/si1.jpg" data-imagezoom="true" class="img-responsive"> </div>
@@ -349,13 +558,11 @@
 			
 </div>
 <!----->
-
-<!-- 点击开始 -->
-
 <div class="clearfix"> </div>
 <div class=" col-md-si">
 						
-			 <script type="text/javascript" src="/dianji/js/Flow.js"></script>
+
+  <script type="text/javascript" src="/dianji/js/Flow.js"></script>
     <script type="text/javascript">
     /* ==== create imageFlow ==== */
     if ($.browser.msie && ($.browser.version == "6.0") && !$.support.style) {
@@ -402,21 +609,84 @@
         </div>
     </div>
 
-<!-- 点击结束 -->
 
 
 
-
-
-
+					
 		</div>
-			<div class="clearfix"></div>
+		
 			</div>			
 		</div>
+
+
+
+
+
+		===========================
 				<!--//products-->
+<div class="social animated wow fadeInUp" data-wow-delay=".1s">
+	<div class="container">
+		<div class="col-sm-3 social-ic">
+			<a href="#">FACEBOOK</a>
+		</div>
+		<div class="col-sm-3 social-ic">
+			<a href="#">TWITTER</a>
+		</div>
+		<div class="col-sm-3 social-ic">
+			<a href="#">GOOGLE+</a>
+		</div>
+		<div class="col-sm-3 social-ic">
+			<a href="#">PINTEREST</a>
+		</div>
+	<div class="clearfix"></div>
+	</div>
+</div>
 
-
-
+<!-- footer -->
+	<div class="footer">
+		<div class="container">
+		<div class="footer-top">
+		<div class="col-md-9 footer-top1">
+		<h4>Duis aute irure dolor in reprehenderit in voluptate </h4>
+		<p>Duis aute irure dolor in reprehenderit in voluptate velit esse.Excepteur sint occaecat cupidatat 
+						non proident Duis aute irure dolor in reprehenderit in voluptate velit esse</p>
+		</div>
+		<div class="col-md-3 footer-top2">
+		<a href="contact.html">Contact Us</a>
+		</div>
+		<div class="clearfix"> </div>
+		</div>
+			<div class="footer-grids">
+				<div class="col-md-4 footer-grid animated wow fadeInLeft" data-wow-delay=".5s">
+					<h3>About Us</h3>
+					<p>Duis aute irure dolor in reprehenderit in voluptate velit esse.<span>Excepteur sint occaecat cupidatat 
+						non proident, sunt in culpa qui officia deserunt mollit.</span></p>
+				</div>
+				<div class="col-md-4 footer-grid animated wow fadeInLeft" data-wow-delay=".6s">
+					<h3>Contact Info</h3>
+					<ul>
+						<li><i class="glyphicon glyphicon-map-marker" ></i>1234k Avenue, 4th block, <span>New York City.</span></li>
+						<li class="foot-mid"><i class="glyphicon glyphicon-envelope" ></i><a href="mailto:info@example.com">info@example.com</a></li>
+						<li><i class="glyphicon glyphicon-earphone" ></i>+1234 567 567</li>
+					</ul>
+				</div>
+				<div class="col-md-4 footer-grid animated wow fadeInLeft" data-wow-delay=".7s">
+				<h3>Sign up for newsletter </h3>
+				<form>
+					<input type="text" placeholder="Email"  required="">
+					<input type="submit" value="Submit">
+				</form>
+			
+				</div>
+			
+				<div class="clearfix"> </div>
+			</div>
+			
+			<div class="copy-right animated wow fadeInUp" data-wow-delay=".5s">
+				<p>Copyright &copy; 2016.Company name All rights reserved.More Templates <a href="http://www.cssmoban.com/" target="_blank" title="模板之家">模板之家</a> - Collect from <a href="http://www.cssmoban.com/" title="网页模板" target="_blank">网页模板</a></p>
+			</div>
+		</div>
+	</div>
 <!-- //footer -->
 <script src="/qiantai/js/imagezoom.js"></script>
 
@@ -433,4 +703,7 @@ $(window).load(function() {
   });
 });
 </script>
-@endsection
+
+
+</body>
+</html>
