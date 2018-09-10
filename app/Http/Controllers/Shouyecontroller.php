290d5ab8afd10556649e6b94a72ac1f360a7a3d6
weidong <65373489@qq.com>
2018-09-10 19:44:51 +0800

    wubug

--- app/Http/Controllers/Shouyecontroller.php
+++ app/Http/Controllers/Shouyecontroller.php
@@ -95,7 +95,7 @@ class Shouyecontroller extends Controller
         }
 
       if($user -> save()){
-            return redirect('/home/people')->with('success','添加成功');
+            return back()->with('success','添加成功');
         }else{
             return back()->with('error','添加失败');
         }
