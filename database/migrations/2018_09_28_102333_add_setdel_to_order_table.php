5c7efc3b8dd648b6dd7455f2afd3ccc445a73195
zebin <1508680645@qq.com>
2018-09-28 10:25:10 +0800

    删除了settings的email字段

---
+++ database/migrations/2018_09_28_102333_add_setdel_to_order_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSetdelToOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            //
+            $table->dropColumn('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            //
+        });
+    }
+}
