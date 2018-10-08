6a296430e7d8c449d36523a74b6b54d727a5ca0d
zebin <1508680645@qq.com>
2018-10-08 09:20:17 +0800

    0

--- resources/views/admin/znx/message_back.blade.php
+++ resources/views/admin/znx/message_back.blade.php
@@ -5,7 +5,7 @@
 <script type="text/javascript" src="/znx/js/qna.js"></script>
 <script type="text/javascript" src="/znx/js/pager.js"></script>
 
-<form action="/doback/{{$message['id']}}" method="post">
+
     {{csrf_field()}} 
     <div class="mainContainner">
         <div class="Corder">
@@ -21,28 +21,29 @@
             </ul>
         </div>
         <div class="commentBox">
-            <div class="Ctop">
-                <label class="Ctitle"><img src="/znx/img/user_comment.png">回复{{$message['send_name']}}:</label>
-            </div>
-            <div class="Cinput">
-                <textarea placeholder="请输入内容" class="Ccont" name="content"></textarea>
-            </div>
+            <form action="/doback/{{$message['id']}}" method="post">
+                <div class="Ctop">
+                    <label class="Ctitle"><img src="/znx/img/user_comment.png">回复{{$message['send_name']}}:</label>
+                </div>
+                <div class="Cinput">
+                    <textarea placeholder="请输入内容" class="Ccont" name="content"></textarea>
+                </div>
 
-            <input type="hidden" name="content_id" value="{{$message['id']}}">
-            <input type="hidden" name="catch_id" value="{{$message['send_id']}}">
-            
-            <div class="Csunbmit">
-                <label class="Cnick"></label>
-                <input type="hidden" name="send_id" value="{{Session::get('id')}}">
-                <input type="hidden" class="Cusername" placeholder="" name="send_name" value="{{Session::get('username')}}"></input>
-                <button class="submitBtn">回复</button>
-
-
-            </div>
+                <input type="hidden" name="content_id" value="{{$message['id']}}">
+                <input type="hidden" name="catch_id" value="{{$message['send_id']}}">
+                
+                <div class="Csunbmit">
+                    <label class="Cnick"></label>
+                    <input type="hidden" name="send_id" value="{{Session::get('id')}}">
+                    <input type="hidden" class="Cusername" placeholder="" name="send_name" value="{{Session::get('username')}}"></input>
+                    <button class="submitBtn">回复</button>
+                </div>
+            </form>
+            <button class="submitBtn" >设为未读</button>
         </div>
     </div>
 </form>
 <form action="/noread/{{$message['id']}}" method="get">
-<button class="submitBtn" >设为未读</button>
-</form>
+
+
 @endsection
